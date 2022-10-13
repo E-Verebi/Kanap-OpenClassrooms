ad6e982b6c4b68dfcf050c2e257e0a9b59819d70
Emile Verebi <emile.verebi@outlook.fr>
2022-10-13 10:27:33 +0200

    Ajout fonction displayProduct et boucle dans fetch / Etape 3 terminée

--- front/js/script.js
+++ front/js/script.js
@@ -2,9 +2,14 @@ fetch('http://localhost:3000/api/products')
     .then((response) => response.json())
     .then((data) => {
         console.log (data)
-        displayProduct(data[0])
+/*La boucle répète jusqu'à ce qu'il n'y ait plus de data[i] dans l'API, son typeof sera donc "undefined" et la boucle se brisera*/
+        for (var i = 0;; i++) {
+            if (typeof data[i] === "undefined") break
+            displayProduct(data[i])
+        }
     });
 
+/*La fonction displayProduct récupère en argument les données d'un produit (récupéré sur l'API grâce à fetch) pour les ajouter à la section "items" de la page d'accueil*/    
 function displayProduct(product) {
     let id = product._id
     let image = product.imageUrl
@@ -19,6 +24,6 @@ function displayProduct(product) {
             <p class="productDescription">${description}</p>
         </article>
     </a> `
-    let productSection = document.getElementById("items")
-    productSection.append(productCard)
+    document.getElementById("items").innerHTML += productCard
+    
 }
