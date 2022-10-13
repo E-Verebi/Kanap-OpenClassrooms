71117b4bb96ed69c6e9083ed7fbfc04a53a177c9
Emile Verebi <emile.verebi@outlook.fr>
2022-10-13 16:15:58 +0200

    Création du product.js et de son contenu, étape 6 terminée

--- front/js/product.js
+++ front/js/product.js
@@ -1,2 +1,26 @@
 let irlId = new URLSearchParams(window.location.search).get('id')
-console.log (irlId)
+console.log(irlId)
+
+fetch(`http://localhost:3000/api/products/${irlId}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data.colors)
+        displayProduct2(data)
+        data.colors.forEach(displayColors)
+    });
+
+function displayProduct2(product) {
+    let img = product.imageUrl
+    let alt = product.altTxt
+    let title = product.name
+    let price = product.price
+    let description = product.description
+    document.getElementsByClassName("item__img")[0].innerHTML = `<img src="${img}" alt="${alt}">`
+    document.getElementById("title").innerHTML = title
+    document.getElementById("price").innerHTML = price
+    document.getElementById("description").innerHTML = description
+}
+
+function displayColors(colorsArray) {
+    document.getElementById("colors").innerHTML += `<option value="${colorsArray}">${colorsArray}</option>`
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,11 @@
 fetch('http://localhost:3000/api/products')
     .then((response) => response.json())
     .then((data) => {
-        console.log (data)
+        console.log(data)
         data.forEach(displayProduct);
     });
 
-/*La fonction displayProduct récupère en argument les données d'un produit (récupéré sur l'API grâce à fetch) pour les ajouter à la section "items" de la page d'accueil*/    
+/*La fonction displayProduct récupère en argument les données d'un produit (récupéré sur l'API grâce à fetch) pour les ajouter à la section "items" de la page d'accueil*/
 function displayProduct(product) {
     let id = product._id
     let image = product.imageUrl
@@ -21,5 +21,5 @@ function displayProduct(product) {
         </article>
     </a> `
     document.getElementById("items").innerHTML += productCard
-    
+
 }
